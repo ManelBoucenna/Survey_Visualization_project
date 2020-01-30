12de2e1e9553bf269297b8e4884ac35e04c43546
ManelBoucenna <manal_maroua.boucenna@g.enp.edu.dz>
2020-01-30 10:04:12 -0500

    Stacked chart v1, need to correct labels

--- client/src/services/visualization_creation/visualizations/StackedChartVisualizationCreator.ts
+++ client/src/services/visualization_creation/visualizations/StackedChartVisualizationCreator.ts
@@ -8,66 +8,61 @@ export class StackedChartVisualizationCreator extends VisualizationDrawer {
   public Draw(): boolean {
     const Entry = this.Entries;
     const id = '#' + Entry.id;
+    const options = Entry.Questions[0].Options;
 
     //dimensions and groups:
-    const years = Entry.ndx.dimension(d => d['IMD1008_YEAR']);
-    const yearsGroup = years.group().reduce(
-      function reduceAdd(p, v) {
-        const variable = Entry.Questions[0].variable;
-        p[variable] = p[variable] + v[variable];
-        p.count = p.count + 1;
-        p.avg = p[variable] / p.count;
-        p.avgTot = p.avgTot + p.avg;
-        p.avgPrcnt = (p.avg / p.avgTot) * 100;
-        if (p.max < v[variable]) { p.max = v[variable]; }
-        p.maxTot = p.maxTot + p.max;
-        p.maxPrcnt = (p.max / p.maxTot) * 100;
+    const variable = Entry.Questions[0].variable;
+    const xdim = Entry.ndx.dimension(d => d['IMD1008_YEAR']);
+    const ydim = xdim.group().reduce(
+      function (p, v) {
+        p[v[variable]] = (p[v[variable]] || 0) + 1;
         return p;
       },
-      function reduceRemove(p, v) {
-        const variable = Entry.Questions[0].variable;
-        p[variable] = p[variable] - v[variable];
-        p.count = p.count - 1;
-        p.avg = p[variable] / p.count;
-        p.avgTot = p.avgTot - p.avg;
-        p.avgPrcnt = (p.avg / p.avgTot) * 100;
-        if (p.max > v[variable]) { p.max = v[variable]; }
-        p.maxTot = p.maxTot - p.max;
-        p.maxPrcnt = (p.max / p.maxTot) * 100;
+      function (p, v) {
+        p[v[variable]] = (p[v[variable]] || 0) - 1;
         return p;
       },
-      function reduceInitial() {
-        const variable = Entry.Questions[0].variable;
-        return {
-          variable: 0, count: 0, avg: 0, avgTot: 0,
-          avgPrcnt: 0, max: 0, maxTot: 0, maxPrcnt: 0
-        };
+      function () {
+        return {};
       });
+
+    function sel_stack(valueKey) {
+      return function (d) {
+        return d.value[valueKey];
+      };
+    }
+
 
     const graph = dc.barChart(id);
     // const width = Entry.width;
     // const height = Entry.height;
     // const margins = Entry.margins;
 
-    const width = 100;
-    const height = 100;
-    const margins = { left: 15, right: 0, top: 0, bottom: 5 };
+    const width = 200;
+    const height = 200;
+    const margins = { left: 50, right: 0, top: 0, bottom: 20 };
+    let start = 0;
+    for (const [key, value] of Object.entries(options)) {
 
-    graph.width(width).height(height).margins(margins)
-      .dimension(years)
-      .group(yearsGroup)
-      .valueAccessor(d => d.value.avgPrcnt)
-      .stack(yearsGroup, 'Survey max %', d => d.value.maxPrcnt)
-      // .x(d3.scale.ordinal().domain(data.map(function (d) {
-      //   return d.channelUUID;
-      // })))
-      .x(d3.scaleBand())
-      .xUnits(dc.units.ordinal)
-      .xAxisLabel('Years')
-      .yAxisLabel('Survey values %')
-      .brushOn(false)
-      .elasticY(true)
-      .renderHorizontalGridLines(true);
+      if (start === 0) {
+        console.log(value, ':', sel_stack(value))
+        graph.width(width).height(height).margins(margins)
+          .dimension(xdim)
+          .group(ydim, value, sel_stack(value))
+          .x(d3.scaleOrdinal().domain(xdim))
+          .xUnits(dc.units.ordinal)
+          .renderHorizontalGridLines(true)
+          .elasticY(true)
+          .elasticX(true)
+          .renderLabel(true)
+          .renderTitle(true)
+        // .legend(dc.legend());
+        start += 1;
+      } else {
+        console.log(value, ':', sel_stack(value));
+        graph.stack(ydim, value, sel_stack(value))
+      }
+    }
 
     graph.render();
     return true;
